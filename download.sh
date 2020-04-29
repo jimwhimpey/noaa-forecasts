e41ce5ae84adac1904829057bc2cffa0476d571f
Jim Whimpey <jwhimpey@slack-corp.com>
2020-04-28 21:24:49 -0700

    It works

---
+++ download.sh
@@ -0,0 +1,2 @@
+curl https://www.cpc.ncep.noaa.gov/products/predictions/814day/814temp.new.gif -o ~/Sites/noaa-forecasts/assets/temp/814temp-$(date +%s).gif
+curl https://www.cpc.ncep.noaa.gov/products/predictions/814day/814prcp.new.gif -o ~/Sites/noaa-forecasts/assets/rain/814prcp-$(date +%s).gif
